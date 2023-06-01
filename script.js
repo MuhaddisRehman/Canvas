b556d162a7cf1c5d2d927be43cc10275df7a71e3
MuhaddisRehman <96332700+MuhaddisRehman@users.noreply.github.com>
2023-06-01 21:39:58 +0500

    modified for touch controls

--- script.js
+++ script.js
@@ -1,111 +1,145 @@
-//////////////CANVAS::DEFAULT::SETTINGS/////////////
-
-
-
-const  canvas = document.getElementById('canvas1')
-const ctx = canvas.getContext('2d')
-canvas.width =  window.innerWidth;
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const particleArray = [];
 let hue = 0;
-window.addEventListener('resize', ()=>{
-    canvas.width =  window.innerWidth;
-    canvas.height = window.innerHeight;
-    
-})
-const mouse = {
-    x:undefined,
-    y:undefined
-}
-canvas.addEventListener('click', (e)=>{
-    mouse.x = e.x;
-    mouse.y = e.y;
-    for (let i = 0; i < 10; i++) {
-        particleArray.push(new Particle())
-    }
-    // drawCircle();
-})
-canvas.addEventListener('mousemove', (e)=>{
-    mouse.x = e.x;
-    mouse.y = e.y;
-    for (let i = 0; i < 2; i++) {
-        particleArray.push(new Particle())
-    }
-})
 
-ctx.lineWidth = 4
+// Create the audio context and oscillator
+const audioContext = new AudioContext();
+const oscillator = audioContext.createOscillator();
+oscillator.frequency.value = 440; // Set the initial frequency
+oscillator.start();
+
+window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
+
+const mouse = {
+    x: undefined,
+    y: undefined,
+    down: false // Track whether the mouse button is down or not
+};
+
+canvas.addEventListener('mousedown', () => {
+    mouse.down = true;
+});
+
+canvas.addEventListener('mouseup', () => {
+    mouse.down = false;
+});
+
+canvas.addEventListener('mousemove', (e) => {
+    if (mouse.down) {
+        mouse.x = e.x;
+        mouse.y = e.y;
+        for (let i = 0; i < 2; i++) {
+            particleArray.push(new Particle());
+        }
+        playSound();
+    }
+});
+
+canvas.addEventListener('touchstart', () => {
+    mouse.down = true;
+});
+
+canvas.addEventListener('touchend', () => {
+    mouse.down = false;
+});
+
+canvas.addEventListener('touchmove', (e) => {
+    e.preventDefault(); // Prevent scrolling on touch devices
+    if (mouse.down) {
+        mouse.x = e.touches[0].clientX;
+        mouse.y = e.touches[0].clientY;
+        for (let i = 0; i < 2; i++) {
+            particleArray.push(new Particle());
+        }
+        playSound();
+    }
+});
+
+ctx.lineWidth = 4;
 ctx.fillStyle = 'white';
 
-
-
-
 class Particle {
-    constructor(){
-        this.x =  mouse.x;
+    constructor() {
+        this.x = mouse.x;
         this.y = mouse.y;
         this.size = Math.random() * 10 + 1;
         this.speedX = Math.random() * 3 - 1.5;
         this.speedY = Math.random() * 3 - 1.5;
-        this.color = `hsl(${hue}, 100%, 50%)`
+        this.color = `hsl(${hue}, 100%, 50%)`;
     }
-    update(){
+
+    update() {
         this.x += this.speedX;
         this.y += this.speedY;
-        if(this.size > 0.3)this.size-= 0.1
+        if (this.size > 0.3) this.size -= 0.1;
     }
-    draw(){
+
+    draw() {
         ctx.fillStyle = this.color;
         ctx.strokeStyle = this.color;
         ctx.beginPath();
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI *2);
-        ctx.fill()
-        ctx.stroke()
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.stroke();
     }
 }
 
-//////////////////////INITIALIZE::PARTICLE::ARRAY::START////////////////////
-
-// const init = ()=>{
-//     for (let i = 0; i < 100; i++) {
-//         particleArray.push(new Particle())
-//     }
-// }
-// init()
-//////////////////////INITIALIZE::PARTICLE::ARRAY::END////////////////////
-
-const handleParticles = () =>{
+const connectParticles = () => {
     for (let i = 0; i < particleArray.length; i++) {
-        const particle = particleArray[i];
         for (let j = i; j < particleArray.length; j++) {
-           
             const dx = particleArray[j].x - particleArray[i].x;
             const dy = particleArray[j].y - particleArray[i].y;
-            const distance = Math.sqrt(dx*dx + dy*dy)
+            const distance = Math.sqrt(dx * dx + dy * dy);
 
-            if(distance<100){
-                ctx.lineWidth = particleArray[j].size/10
-                ctx.beginPath()
-                ctx.moveTo(particleArray[j].x, particleArray[j].y)
-                ctx.lineTo(particleArray[i].x, particleArray[i].y)
+            if (distance < 200) {
+                const opacityValue = 1 - distance / 100;
+                ctx.strokeStyle = `hsla(${hue}, 100%, 50%, ${opacityValue})`;
+                ctx.lineWidth = particleArray[j].size / 10;
+                ctx.beginPath();
+                ctx.moveTo(particleArray[j].x, particleArray[j].y);
+                ctx.lineTo(particleArray[i].x, particleArray[i].y);
                 ctx.stroke();
             }
         }
+    }
+};
+
+const handleParticles = () => {
+    for (let i = 0; i < particleArray.length; i++) {
+        const particle = particleArray[i];
         if (particle.size < 0.9) {
-            particleArray.splice(i, 1)
+            particleArray.splice(i, 1);
             i--;
         }
-        particle.update()
-        particle.draw()
+        particle.update();
+        particle.draw();
     }
-}
+};
 
-const animate = () =>{
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    // ctx.fillStyle = 'rgba(0, 0, 0, 0.01)'
-    // ctx.fillRect(0, 0, canvas.width, canvas.height)
-
-    handleParticles()
+const animate = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    handleParticles();
+    connectParticles();
     hue++;
-    requestAnimationFrame(animate)
+    requestAnimationFrame(animate);
+};
+
+animate();
+
+// Function to play the sound
+function playSound() {
+    const gainNode = audioContext.createGain();
+    oscillator.connect(gainNode);
+    gainNode.connect(audioContext.destination);
+    gainNode.gain.setValueAtTime(0.1, audioContext.currentTime);
+    gainNode.gain.exponentialRampToValueAtTime(0.01, audioContext.currentTime + 0.5);
+    setTimeout(() => {
+        oscillator.stop();
+    }, 1000);
 }
-animate()
